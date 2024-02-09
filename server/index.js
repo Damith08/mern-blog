e690502c786a8ae87bc6ad430495dab816716143
Damith08 <dmthsamarasinghe@gmail.com>
2024-02-09 07:41:21 +0530

    feat: check if looged in and add logout functionality

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const User = require("./models/User");
 const bcrypt = require("bcrypt");
 const app = express();
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "aklhsdfjkioaeio";
 
 app.use(cors({ credentials: true, origin: "http://localhost:4200" }));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect("mongodb://localhost:27017");
 
@@ -42,6 +44,18 @@ app.post("/login", async (req, res) => {
   } else {
     res.status(400).json("wrong credential");
   }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
 });
 
 app.listen(4000);
